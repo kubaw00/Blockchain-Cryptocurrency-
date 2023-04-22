f4c8bc9d4f582c715317cc1449074c5ce6f6118c
Jakub Widurek <jakubwidurek@gmail.com>
2023-04-22 17:56:03 +0200

    create post request to mine block

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const Blockchain = require('./blockchain');
 
+const Blockchain = require('./blockchain');
 const app = express();
 
 const blockchain = new Blockchain();
@@ -8,6 +9,12 @@ const PORT = 5000;
 
 app.get('/api/blocks', (req, res) => {
   res.send(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  res.redirect('/api/blocks');
 });
 
 app.listen(PORT, () => console.log(`Listen on post ${PORT}`));
