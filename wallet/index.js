850cddf3ed62af0e8da884253fe9c138fc729bbd
Jakub Widurek <jakubwidurek@gmail.com>
2023-04-25 23:59:09 +0200

    sign data and veryfying signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,12 +1,16 @@
 const { INITIAL_BALLANCE } = require('../genesis');
 const { ec } = require('../util');
+const crytpoHash = require('../util/crypto-hash');
 
 class Wallet {
   constructor() {
     this.balance = INITIAL_BALLANCE;
 
-    const keyPair = ec.genKeyPair();
+    this.keyPair = ec.genKeyPair();
     this.publicKey = keyPair.getPublic().encode('hex');
+  }
+  sign(data) {
+    return this.keyPair.sign(crytpoHash(data));
   }
 }
 
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,3 +1,4 @@
+const { verifySignature } = require('../util');
 const Wallet = require('./index');
 
 describe('wallet', () => {
@@ -13,5 +14,29 @@ describe('wallet', () => {
 
   test('to have balance', () => {
     expect(wallet).toHaveProperty('balance');
+  });
+
+  describe('signing data', () => {
+    const data = 'foobar';
+
+    test('verifies signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data),
+        })
+      ).toBe(true);
+    });
+
+    test('not verify invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data),
+        })
+      ).toBe(false);
+    });
   });
 });
